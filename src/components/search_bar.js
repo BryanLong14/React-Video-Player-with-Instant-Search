e4e2fa0c54c7bd0b14769718b6994187ab4f03db
Bryan Long <BryanLong1@gmail.com>
2018-04-24 14:22:04 -0600

    Added state and setState for input bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        {<br />} The value of the input search bar is: {this.state.term}
+      </div>
+    );
   }
 }
 
