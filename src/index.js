c23a6823df00ad76f0c0e263fd7832fd9bdc9dee
Bryan Long <BryanLong1@gmail.com>
2018-04-29 17:32:51 -0600

    Added React State Updated for SearchBar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_key = "AIzaSyDj06xC1hn7XkzfLJdPUgcbmlbVIKksn_8";
 
@@ -11,18 +12,29 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_key, term: "JavaScript Tutorials" }, videos => {
-      this.setState({ videos });
+    this.videoSearch("Colorado Wilderness");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_key, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
       </div>
     );
   }
